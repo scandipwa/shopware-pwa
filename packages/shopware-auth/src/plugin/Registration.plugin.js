51cadf2e588d93e534b6c00413fdfac28757f8e3
yeegor <yegor.batov@gmail.com>
2021-05-10 18:34:28 +0300

    Add registration page

---
+++ packages/shopware-auth/src/plugin/Registration.plugin.js
@@ -0,0 +1,9 @@
+import Registration from '../component/Registration';
+
+const addRegisterPage = () => <Registration />;
+
+export default {
+    'Pages/register/Page': {
+        function: addRegisterPage
+    }
+};
