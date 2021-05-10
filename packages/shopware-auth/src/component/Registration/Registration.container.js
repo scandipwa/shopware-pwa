98d887143bacd15577a59bab0f6f7743a03f7d76
yeegor <yegor.batov@gmail.com>
2021-05-10 18:51:58 +0300

    Fix broken container functions

--- packages/shopware-auth/src/component/Registration/Registration.container.js
+++ packages/shopware-auth/src/component/Registration/Registration.container.js
@@ -6,8 +6,7 @@ import RegistrationProvider from './Registration.provider';
 /** @namespace ShopwareAuth/Component/Registration/Container/RegistrationContainer */
 export class RegistrationContainer extends PureComponent {
     containerFunctions = {
-        handleSubmitSuccess: this.handleSubmitSuccess.bind(this),
-        handleSubmitError: this.handleSubmitError.bind(this)
+        handleSubmit: this.handleSubmit.bind(this)
     };
 
     handleSubmit(event) {
