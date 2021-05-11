6fb01b3d839f27cd34954fdf7df71ddb70aad4b9
Alfreds Genkins <alfreds@scandiweb.com>
2021-05-11 14:07:04 +0300

    Fixed trailing slash issue

--- packages/seo-url/src/plugin/SeoUrl.plugin.js
+++ packages/seo-url/src/plugin/SeoUrl.plugin.js
@@ -21,9 +21,30 @@ const addRenderSeoUrlEntityPage = ([{
     );
 };
 
+const getSeoUrlConsideringSlashes = async (seoPathInfo) => {
+    const currentUrl = await getSeoUrlBySeoPathInfo(seoPathInfo);
+
+    if (currentUrl) {
+        return currentUrl;
+    }
+
+    if (seoPathInfo.endsWith('/')) {
+        // try without a slash
+        return getSeoUrlBySeoPathInfo(
+            seoPathInfo.substring(0, seoPathInfo.length() - 1)
+        );
+    }
+
+    // try with a slash
+    return getSeoUrlBySeoPathInfo(
+        `${seoPathInfo }/`
+    );
+};
+
 const addGetSeoUrlProps = async ([{ res, resolvedUrl }]) => {
     const seoPathInfo = resolvedUrl.substring(1);
-    const seoUrl = await getSeoUrlBySeoPathInfo(seoPathInfo);
+
+    const seoUrl = await getSeoUrlConsideringSlashes(seoPathInfo);
 
     if (!seoUrl) {
         // eslint-disable-next-line no-param-reassign
