36eb5ea1f6373b18252911e5ed05f5d54d22b508
yeegor <yegor.batov@gmail.com>
2021-05-14 16:59:05 +0300

    Add CartPage route

---
+++ packages/cart/src/plugin/CartPage.plugin.js
@@ -0,0 +1,9 @@
+import CartPage from '../component/CartPage';
+
+const addCartPage = () => <CartPage />;
+
+export default {
+    'Pages/checkout/cart/Page': {
+        function: addCartPage
+    }
+};
