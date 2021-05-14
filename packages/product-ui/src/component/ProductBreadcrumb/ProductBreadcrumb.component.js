d0fa2d19ff6c96df11d19145ed8d1efa6ec1a336
Okabe Rintaro <ejnshtein@gmail.com>
2021-05-14 16:47:02 +0300

    make breadcrumbs on product page work

--- packages/product-ui/src/component/ProductBreadcrumb/ProductBreadcrumb.component.js
+++ packages/product-ui/src/component/ProductBreadcrumb/ProductBreadcrumb.component.js
@@ -1,6 +1,7 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-/* eslint-disable @scandipwa/scandipwa-guidelines/only-render-in-component */
+/* eslint-disable @scandipwa/scandipwa-guidelines/jsx-no-conditional,react/jsx-one-expression-per-line, @scandipwa/scandipwa-guidelines/only-render-in-component */
+import { API_ENDPOINT } from '@scandipwa/framework/src/util/Client';
 import ProductContext from '@scandipwa/product/src/context/Product.context';
+import Link from 'next/link';
 import { PureComponent } from 'react';
 
 /** @namespace Productui/Component/ProductBreadcrumb/Component/ProductBreadcrumbComponent */
@@ -17,17 +18,38 @@ export class ProductBreadcrumbComponent extends PureComponent {
     /**
      * @param {string} breadcrumb
      */
+    getBreadcrumbUrl(breadcrumb) {
+        const { pathname } = new URL(breadcrumb.replace(/\s/i, '-'), API_ENDPOINT);
+
+        return pathname;
+    }
+
+    /**
+     * @param {string} breadcrumb
+     */
     renderBreadCrumb = (breadcrumb, i) => (
-        <li key={ i } style={ { marginRight: '0.5rem' } }> &gt; { breadcrumb }</li>
+            <li key={ i } style={ { marginRight: '0.5rem' } }>
+                { i === 0 ? '' : '/' } <Link href={ this.getBreadcrumbUrl(breadcrumb) }>{ breadcrumb }</Link>
+            </li>
     );
 
     renderBreadCrumbs() {
         const { product: { product: { seoCategory: { breadcrumb } } } } = this.getContextValue();
 
+        const filteredBreadCrumbs = breadcrumb.filter((bc) => {
+            const navigationUrls = ['home', 'main navigation'];
+
+            if (navigationUrls.includes(bc.toLowerCase().trim())) {
+                return false;
+            }
+
+            return true;
+        });
+
         return (
             <div>
                 <ul style={ { display: 'flex', listStyleType: 'none', padding: 0 } }>
-                    { breadcrumb.map(this.renderBreadCrumb) }
+                    { filteredBreadCrumbs.map(this.renderBreadCrumb) }
                 </ul>
             </div>
         );
