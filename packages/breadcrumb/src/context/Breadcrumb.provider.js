d9f3a54ae21a403b93f3c376b5f0769b5a553378
ejnshtein <ejnshtein@gmail.com>
2021-05-19 15:27:49 +0300

    move product-breadcrumb logic to product-breadcrumb module

--- packages/breadcrumb/src/context/Breadcrumb.provider.js
+++ packages/breadcrumb/src/context/Breadcrumb.provider.js
@@ -1,8 +1,6 @@
 /* eslint-disable react/destructuring-assignment */
 import NextPageContext from '@scandipwa/framework/src/context/NextPage.context';
-import { arrayCompare } from '@scandipwa/framework/src/util/Compare';
 
-import { getCategoryById } from '../../../category/src/api/Category.request';
 import { ContextProvider, withProvider } from '../../../framework/src/util/Context';
 import BreadcrumbContext from './Breadcrumb.context';
 
@@ -23,12 +21,7 @@ export class BreadcrumbProvider extends ContextProvider {
          */
         const {
             props: {
-                product: {
-                    product: {
-                        seoCategory
-                    } = {}
-                } = {},
-                breadcrumbCategories: breadcrumbCategoriesFromServer
+                breadcrumbCategories: breadcrumbCategoriesFromServer = []
             }
         } = this.context;
 
@@ -40,68 +33,11 @@ export class BreadcrumbProvider extends ContextProvider {
             this.setState({
                 breadcrumbCategories: breadcrumbCategoriesFromServer
             });
-
-            return;
-        }
-
-        if (seoCategory && breadcrumbCategories.length === 0) {
-            this.requestBreadcrumbCategories();
         }
     }
 
-    componentDidUpdate() {
-        if (!this.getIsCurrentBreadcrumbCategoriesMatchingProductBreadcrumbCategories()) {
-            this.requestBreadcrumbCategories();
-        }
-    }
-
-    getIsCurrentBreadcrumbCategoriesMatchingProductBreadcrumbCategories() {
-        /**
-         * @type {React.ContextType<typeof NextPageContext>}
-         */
-        const {
-            props: {
-                product: {
-                    product: {
-                        seoCategory: {
-                            path: seoCategoryPath
-                        } = {}
-                    } = {}
-                } = {}
-            }
-        } = this.context;
-        const { breadcrumbCategories } = this.state;
-
-        const breadcrumbCategoryIds = seoCategoryPath
-            .split('|')
-            .filter(Boolean);
-
-        const storedCategoryIds = breadcrumbCategories.map(({ id }) => id);
-
-        return arrayCompare(storedCategoryIds, breadcrumbCategoryIds);
-    }
-
-    _requestBreadcrumbCategories() {
-        /**
-         * @type {React.ContextType<typeof NextPageContext>}
-         */
-        const {
-            props: {
-                product: {
-                    product: {
-                        seoCategory: {
-                            path: seoCategoryPath
-                        } = {}
-                    } = {}
-                } = {}
-            }
-        } = this.context;
-
-        const breadcrumbCategories = seoCategoryPath
-            .split('|')
-            .filter(Boolean);
-
-        return Promise.all(breadcrumbCategories.map((categoryId) => getCategoryById(categoryId)));
+    async _requestBreadcrumbCategories() {
+        return [];
     }
 
     async requestBreadcrumbCategories() {
--- packages/product-breadcrumb/src/plugin/BreadcrumbCategoriesOnProductRequest.plugin.js
+++ packages/product-breadcrumb/src/plugin/BreadcrumbCategoriesOnProductRequest.plugin.js
@@ -1,7 +1,65 @@
 import { getCategoryById } from '@scandipwa/category/src/api/Category.request';
+import NextPageContext from '@scandipwa/framework/src/context/NextPage.context';
+import { arrayCompare } from '@scandipwa/framework/src/util/Compare';
 
-// eslint-disable-next-line @scandipwa/scandipwa-guidelines/use-namespace
-export const addBreadcrumbCategoriesProps = async (args, callback) => {
+const getIsCurrentBreadcrumbCategoriesMatchingProductBreadcrumbCategories = ({ state, context }) => {
+    /**
+     * @type {React.ContextType<typeof NextPageContext>}
+     */
+    const {
+        props: {
+            product: {
+                product: {
+                    seoCategory: {
+                        path: seoCategoryPath
+                    } = {}
+                } = {}
+            } = {}
+        }
+    } = context;
+    const { breadcrumbCategories } = state;
+
+    const breadcrumbCategoryIds = seoCategoryPath
+        .split('|')
+        .filter(Boolean);
+
+    const storedCategoryIds = breadcrumbCategories.map(({ id }) => id);
+
+    return arrayCompare(storedCategoryIds, breadcrumbCategoryIds);
+};
+
+const requestCategoryProductsIfPossible = async (args, callback, instance) => {
+    callback(...args);
+
+    const { context, state } = instance;
+
+    if (!getIsCurrentBreadcrumbCategoriesMatchingProductBreadcrumbCategories({ state, context })) {
+        instance.requestBreadcrumbCategories();
+    }
+};
+
+const requestProductBreadcrumbCategories = async (args, callback, instance) => {
+    await callback(...args);
+    const {
+        props: {
+            product: {
+                product: {
+                    seoCategory: {
+                        path: seoCategoryPath
+                    } = {}
+                } = {}
+            } = {}
+        }
+    } = instance.context;
+
+    const breadcrumbCategories = seoCategoryPath
+        .split('|')
+        .filter(Boolean);
+
+    return Promise.all(breadcrumbCategories.map((categoryId) => getCategoryById(categoryId)));
+};
+
+export const addBreadcrumbCategoriesServerSideProps = async (args, callback) => {
     const { props: originalProps } = await callback(...args);
 
     if (!originalProps.product) {
@@ -35,10 +93,17 @@ export const addBreadcrumbCategoriesProps = async (args, callback) => {
 };
 
 export default {
+    'Breadcrumb/Context/Breadcrumb/Provider/BreadcrumbProvider': {
+        'member-function': {
+            componentDidMount: requestCategoryProductsIfPossible,
+            componentDidUpdate: requestCategoryProductsIfPossible,
+            _requestBreadcrumbCategories: requestProductBreadcrumbCategories
+        }
+    },
     'Pages/[[...seoPathInfo]]/getServerSideProps': {
         function: {
-            implementation: addBreadcrumbCategoriesProps,
-            position: 11000
+            implementation: addBreadcrumbCategoriesServerSideProps,
+            position: 11000 // we expect that product props already injected by other plugin
         }
     }
 };
