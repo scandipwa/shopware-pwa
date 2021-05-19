a1733223728ba77bfc83cdb643290fbdd81476a4
ejnshtein <ejnshtein@gmail.com>
2021-05-19 18:15:08 +0300

    show active color on last breadcrumb

--- packages/material-ui/src/component/Breadcrumb/Breadcrumb.container.tsx
+++ packages/material-ui/src/component/Breadcrumb/Breadcrumb.container.tsx
@@ -5,17 +5,26 @@ import { PureComponent } from 'react';
 
 /** @namespace Materialui/Component/Breadcrumb/Container/BreadcrumbContainer */
 export class BreadcrumbContainer extends PureComponent<BreadcrumbProps> {
-    renderBreadCrumb = (breadcrumb: BreadcrumbItem, i: number): JSX.Element => {
+    renderBreadCrumb = (breadcrumb: BreadcrumbItem, i: number, array: BreadcrumbItem[]): JSX.Element => {
+        const isLastBreadcrumb = i === array.length - 1;
         if (breadcrumb.href) {
             return (
-                <Link key={ i } href={ breadcrumb.href } onClick={ breadcrumb.onClick }>
+                <Link
+                  key={ i }
+                  href={ breadcrumb.href }
+                  onClick={ breadcrumb.onClick }
+                  color={ isLastBreadcrumb ? 'textPrimary' : 'inherit' }
+                >
                     { breadcrumb.label }
                 </Link>
             );
         }
 
         return (
-            <Typography key={ i }>
+            <Typography
+              key={ i }
+              color={ isLastBreadcrumb ? 'textPrimary' : 'inherit' }
+            >
                 { breadcrumb.label }
             </Typography>
         );
